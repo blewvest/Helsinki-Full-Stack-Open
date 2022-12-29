bf1b6dc37bc4a26411634d242342024addc3ad4c
Itai Epstein <itaiepstein@Itais-MacBook-Pro.local>
2022-12-29 18:11:57 -0500

    1/3 done with 1d

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,6 +4,8 @@
 //   )
 // }
 
+import { useState } from "react"
+
 // const Part = (props) => {
 //   return (
 //     <div>
@@ -77,35 +79,77 @@
 // }
 
 /*PART C*/
-import { useState } from "react"
+// import { useState } from "react"
 
-const Display = ({ counter }) => ( <div>{counter}</div> )
+// const Display = ({ counter }) => ( <div>{counter}</div> )
 
-const Button = ({ onClick, text }) => {<button onClick={onClick}>{text}</button> }
+// const Button = ({ onClick, text }) => {<button onClick={onClick}>{text}</button> }
 
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
+// const App = (props) => {
+//   const [ counter, setCounter ] = useState(0)
 
-  // setInterval(
-  //   () => setCounter(counter + 1), 1000
-  // )
+//   // setInterval(
+//   //   () => setCounter(counter + 1), 1000
+//   // )
 
-  // console.log('rendering...', counter)
+//   // console.log('rendering...', counter)
 
-  // const handleClick = () => {
-  //   console.log('clicked')
-  // }
+//   // const handleClick = () => {
+//   //   console.log('clicked')
+//   // }
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter}></Display>
+//       <Button onClick={increaseByOne} text='plus'></Button>
+//       <Button onClick={setToZero} text='zero'></Button>
+//       <Button onClick={decreaseByOne} text='minus'></Button>
+//     </div>
+//   )
+// }
+
+/* PART D */
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  }
+  return (
+    <div>
+      button press history : {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => ( <button onClick={onClick}>{text}</button> )
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => { 
+    setAll(allClicks.concat('L'))  
+    setLeft(left + 1 )
+  }
+
+  const handleRightClick = () => { 
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+   }
 
   return (
     <div>
-      <Display counter={counter}></Display>
-      <Button onClick={increaseByOne} text='plus'></Button>
-      <Button onClick={setToZero} text='zero'></Button>
-      <Button onClick={decreaseByOne} text='minus'></Button>
+      {left}
+      <Button onClick={handleLeftClick} text='left' /> 
+      <Button onClick={handleRightClick} text='left'/>
+      {right}
+      <History allClicks={allClicks}></History>
     </div>
   )
 }
